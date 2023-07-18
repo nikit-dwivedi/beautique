f6674b6ece725b786cda2b6aab583a212c7b8a77
nikitdwivedi <nikitdwivedi1997@gmail.com>
2023-07-18 19:41:03 +0530

    self signed cert

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-import { createServer } from 'http';
+import { createServer as createHttpServer } from 'http';
 import app from './app.js';
 import {config} from "dotenv";
-import { randomBytes } from 'crypto';
+import { readFileSync } from 'fs';
+import { createServer as createHttpsServer } from 'https';
 config()
 const port = process.env.PORT||9000;
-const server = createServer(app)
+const sPort = process.env.SPORT||7889;
+
+var privateKey  = readFileSync('sslcert/server.key', 'utf8');
+var certificate = readFileSync('sslcert/server.crt', 'utf8');
+
+let credentials = {key: privateKey, cert: certificate};
+
+const server = createHttpServer(app)
+const secureServer = createHttpsServer(credentials,app);
+
 server.listen(port, () => {
     console.log(`server running on ${port}`);
+})
+
+secureServer.listen(sPort, () => {
+    console.log(`Secure server listening at ${sPort}`);
 })
